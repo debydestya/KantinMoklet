cc17bfc3f037532a100f160cef67b6c7e73b54f0
DEBY <debydestyananda5c@gmail.com>
2016-10-12 14:45:40 +0700

    Intro Splash

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl206152433/kantinmoklet/SplashScreen.java
@@ -0,0 +1,40 @@
+package id.sch.smktelkom_mlg.project.xirpl206152433.kantinmoklet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vamsikrishna on 12-Feb-15.
+ */
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
