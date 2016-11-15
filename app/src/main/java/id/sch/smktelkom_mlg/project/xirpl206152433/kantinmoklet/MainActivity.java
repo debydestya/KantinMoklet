68a5c3cfa6a4e0f3c4efb7a5282715979b392dab
Rizaldi Wahaz <rizaldi.wahaz@gmail.com>
2016-11-15 21:17:55 +0700

    SlideShow

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl206152433/kantinmoklet/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl206152433/kantinmoklet/MainActivity.java
@@ -2,12 +2,19 @@ package id.sch.smktelkom_mlg.project.xirpl206152433.kantinmoklet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewFlipper viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewFlipper = (ViewFlipper) this.findViewById(R.id.viewFlipper);
+        viewFlipper.startFlipping();
+        viewFlipper.setFlipInterval(2300);
     }
 }
