54ef6f34ae0499d2791e2b4622568196ab21b9cd
Rizaldi Wahaz <rizaldi.wahaz@gmail.com>
2016-11-29 09:36:14 +0700

    Welcome Screen (Intro Slider)

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl206152433/kantinmoklet/model/food.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl206152433/kantinmoklet/model/food.java
@@ -2,7 +2,6 @@ package id.sch.smktelkom_mlg.project.xirpl206152433.kantinmoklet.model;
 
 import java.io.Serializable;
 
-
 public class food implements Serializable {
     public String judulf;
     public String deskripsif;
